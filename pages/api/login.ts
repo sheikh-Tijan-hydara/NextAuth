0f84f8d0e0384b237bbc0f16dd098c22714e7a05
sheikh-Tijan-hydara <hydarasheikhtijan30@gmail.com>
2024-03-15 13:22:55 +0000

    Commented out method check in login API

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -6,7 +6,7 @@ import { compareSync} from 'bcrypt';
 
 const db = getFirestore(app);
 export default async function handleer( req: NextApiRequest, res: NextApiResponse ) {
-    if(req.method !== "POST") return res.status(405).json({ message: "Method not allowed" });
+    // if(req.method !== "POST") return res.status(405).json({ message: "Method not allowed" });
     if(!req.body.username || !req.body.password) return res.status(400).json({ message: "Please fill in all fields" });
     if(req.body.password.length < 6) return res.status(400).json({ message: "Password must be at least 6 characters" });
     try{
