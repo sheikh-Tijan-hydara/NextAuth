9f835dbcb309e4c85054123c466d9dd9d05168ad
sheikh-Tijan-hydara <hydarasheikhtijan30@gmail.com>
2024-03-15 11:51:28 +0000

    Update initial state values in Signup component

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -5,10 +5,10 @@ import { useState } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Signup() {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [username, setUsername] = useState(" ");
+  const [email, setEmail] = useState(" ");
+  const [password, setPassword] = useState(" ");
+  const [confirmPassword, setConfirmPassword] = useState(" ");
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
