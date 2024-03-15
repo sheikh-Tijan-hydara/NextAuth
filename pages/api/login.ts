2e8115cca82889dda27055c3b0bcaa3c4f1f02b2
sheikh-Tijan-hydara <hydarasheikhtijan30@gmail.com>
2024-03-15 13:38:53 +0000

    Refactor login API handler

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,40 +1,54 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import app from '../../firebase/clientApp'; 
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-import { compareSync} from 'bcrypt';
-
+import app from "../../firebase/clientApp";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import { compareSync } from "bcrypt";
 
 const db = getFirestore(app);
-export default async function handleer( req: NextApiRequest, res: NextApiResponse ) {
-    if(req.method !== "POST") {
-        console.log("Request method:", req.method);
-        return res.status(405).json({ message: "Method not allowed" });
-    }
-    if(!req.body.username || !req.body.password) return res.status(400).json({ message: "Please fill in all fields" });
-    if(req.body.password.length < 6) return res.status(400).json({ message: "Password must be at least 6 characters" });
-    try{
-        const usersCollection = collection(db, 'users');
-        const userSnapshot = await getDocs(usersCollection);
-        let foundUser = false; 
-        userSnapshot.forEach(async (doc) => {
-            if (doc.data().username === req.body.username) {
-                const isPasswordMatch = compareSync(req.body.password, doc.data().password);
-                if (isPasswordMatch) {
-                    res.status(200).json({ message: "Successfully logged in", user: { username: doc.data().username, email: doc.data().email } });
-                    console.log("Successfully logged in", doc.data().username, doc.data().email);
-                    foundUser = true;
-                }
-            }
-        });
-
-        if (!foundUser) {
-            res.status(400).json({ message: "Invalid credentials" });
+export default async function handleer(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method not allowed" });
+  }
+  if (!req.body.username || !req.body.password)
+    return res.status(400).json({ message: "Please fill in all fields" });
+  if (req.body.password.length < 6)
+    return res
+      .status(400)
+      .json({ message: "Password must be at least 6 characters" });
+  try {
+    const usersCollection = collection(db, "users");
+    const userSnapshot = await getDocs(usersCollection);
+    let foundUser = false;
+    userSnapshot.forEach(async (doc) => {
+      if (doc.data().username === req.body.username) {
+        const isPasswordMatch = compareSync(
+          req.body.password,
+          doc.data().password
+        );
+        if (isPasswordMatch) {
+          res.status(200).json({
+            message: "Successfully logged in",
+            user: { username: doc.data().username, email: doc.data().email },
+          });
+          console.log(
+            "Successfully logged in",
+            doc.data().username,
+            doc.data().email
+          );
+          foundUser = true;
         }
+      }
+    });
 
-    }catch(error){
-        console.error("Error logging in user: ", error);
-        res.status(500).json({ message: "Error logging in user" });
+    if (!foundUser) {
+      res.status(400).json({ message: "Invalid credentials" });
     }
+  } catch (error) {
+    console.error("Error logging in user: ", error);
+    res.status(500).json({ message: "Error logging in user" });
+  }
 
-    res.status(200).json({ message: "Successfully logged in" });    
+  res.status(200).json({ message: "Successfully logged in" });
 }
